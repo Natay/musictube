77f3779ed761da7f7d1ee1f3cb4f163a04efdffd
Natay <b.natay@gmail.com>
2017-05-12 14:47:13 -0400

    adding things

--- downloader.py
+++ downloader.py
@@ -16,19 +16,90 @@ import youtube_dl
 
 
 
+def myhook(e):
+	
+	if e['status'] == 'finished':
+		print('Download complete, converting to mp3')
 
-def extract_infot(yt_url):
-	return NotImplemented 
 
-def download(yt_url):
 
-	ydl_opts = {
-		'format': 'bestaudio/best',
-		'postprocessors': [{
-			'key': 'FFmpegExtractAudio',
-			'preferredcodec': 'mp3',
-			'preferredquality': '192'
-				}],
-		}
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    		ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+
+def save(mp3, path):
+	# last thing to work on 
+	return NotImplementedError 
+
+
+
+
+
+class Download(object):
+
+
+	def __init__(self, yt_url):
+
+		self.link = yt_url
+
+		self.opts = {
+			'format': 'bestaudio/best',
+			'postprocessors': [{
+				'key': 'FFmpegExtractAudio',
+				'preferredcodec': 'mp3',
+				'preferredquality': '192'
+					}],
+			'progress_hooks': [myhook],
+			}
+
+	def info(self):
+
+		"""
+		Extract page info without downloading link 
+		
+		"""
+
+		return NotImplemented 
+
+
+
+	def download(self):
+
+
+		"""
+		Download link given
+
+		"""
+
+		with youtube_dl.YoutubeDL(self.opts) as ydl:
+	    		ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+
+
+if __name__ == '__main__':
+	
+	testlinks = ['https://www.youtube.com/watch?v=vxa-RHeOc0w',
+		    'https://www.youtube.com/watch?v=qkxEKumTj08' ]
+
+	
+	for link in testlinks:
+
+		dl = Download(link)
+
+		print(dl.info()) # print info 
+		
+		
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
