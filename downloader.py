dff8c46ed42794799995544c3949a8eec837a1d8
Admin Buddy <admin@Admins-MacBook-Air.local>
2018-06-15 17:28:13 -0400

    update

--- downloader.py
+++ downloader.py
@@ -4,47 +4,51 @@
 main module extracting mp3
 
 input:
-	-link: youtube link with music wanted 
-	-dest: destination directory
+-link: youtube link with music wanted
+-dest: destination directory
 
 ouputs:
 
-	-mp3 file in specified directory
+-mp3 file in specified directory
 
 """
 
 
-from __future__ import unicode_literals 
+from __future__ import unicode_literals
 import sys
 
 import youtube_dl
 
 OPTS = {
-	'format': 'bestaudio/best',
-	'postprocessors': [{
-			'key': 'FFmpegExtractAudio',
-			'preferredcodec': 'mp3',
-			'preferredquality': '192'
-			}],
-	'forcefilename': True,
-	}
+'format': 'bestaudio/best',
+'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192'
+		}],
+'forcefilename': True,
+}
 
 
 def main():
-	
-	# hardcoded destination and links files 
-	dest = '/home/natay/Desktop/mymusic/'
-	urlfile = dest + 'links.txt'
 
-	links = urls_from_file(urlfile)
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
+	parser.add_argument("links", type=str, help="file with links")
+	parser.add_argument("dest_dir", type=str, help="dir to download to")
+
+	args = parser.parse_args()
+
+	links = urls_from_file(args.links)
 
 	for link in links:
-		
-		download(link, dest)
-	
+
+		download(link, args.dest_dir)
+
 
 def myhook(e):
-	
+
 	if e['status'] == 'finished':
 		print('DOWNLOAD COMPLETE, converting to mp3')
 
@@ -56,11 +60,11 @@ def urls_from_file(txtfile):
 		for one_link in links:
 			yield one_link
 
- 
+
 def download(link, dest):
 
 	global OPTS
-	
+
 	OPTS['outtmpl'] = dest + '%(title)s.%(ext)s'
 	OPTS['progress_hooks']= [myhook]
 
@@ -69,13 +73,12 @@ def download(link, dest):
 		ydl.download([link])
 
 	print('-'*100)
-	
-	
+
+
 
 if __name__ == '__main__':
-
 	main()
-		
+
 
 
 
